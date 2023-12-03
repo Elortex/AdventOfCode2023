6aae4d87505eef813e9713240357df34f7ea23da
Mateusz <mateuszhumeniuk@wp.pl>
2023-12-03 18:39:51 +0100

    add input, create silverStar.py

--- Day2/silverStar.py
+++ Day2/silverStar.py
@@ -1,4 +1,5 @@
 from sys import argv
+import re
 
 def main():
     if len(argv) < 2:
@@ -6,6 +7,32 @@ def main():
         exit(1)
     filename = argv[1]
     my_list = [line for line in open(filename)]
+
+    cubeDict = {
+        'red' : 12,
+        'green' : 13,
+        'blue' : 14
+        }
+
+    sumLegalID = 0
+    for i in range(len(my_list)):
+        currGame = re.sub("Game \d: |Game \d\d: |Game \d\d\d: ", "", my_list[i])
+        currGame = re.split(';|:', currGame)
+        isCurrGameLegal = True
+        
+        for subGame in currGame:
+            splitSubGame = re.sub(",", "", subGame)
+            splitSubGame = splitSubGame.strip()
+            splitSubGame = re.split(" ", splitSubGame)
+
+            for y in range(0, len(splitSubGame), 2):
+                if int(splitSubGame[y]) > cubeDict[splitSubGame[y + 1]]:
+                    isCurrGameLegal = False
+        
+        if isCurrGameLegal:
+            sumLegalID += i + 1
+
+    print(sumLegalID)
 
 if __name__ == "__main__":
     main()
